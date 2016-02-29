84f2c98009d78b5f33f57efaae74d6cd4be74d99
Thomas Baruchel <baruchel@gmx.com>
2016-02-29 10:50:04 +0100

    Added some comments.

--- julia/recvec.jl
+++ julia/recvec.jl
@@ -12,8 +12,9 @@ function recvec(v; compute=BigInt, output=Int64)
   sq2 = 1
   q  = Array(Rational{compute}, n)
   m  = Array(Rational{compute}, n)
+  b = 1
   while sq2 <= z
-    b = 1
+    # look for first non zero term in the series taylor(l)
     while l[b] == 0
       if (b += 1) > sl
         c = 1
@@ -25,6 +26,7 @@ function recvec(v; compute=BigInt, output=Int64)
         return q
       end
     end
+    # reciprocal of the series: x^(b-1) / taylor(l)
     m[1] = 1 // l[b]
     m[2:sl] = 0
     sm = 1
@@ -37,6 +39,7 @@ function recvec(v; compute=BigInt, output=Int64)
     end
     l, m = m, l
     sl = sm
+    # compute (only) denominator of Pade approximant
     q[1:sq2] = l[1] * q2[1:sq2]
     q[sq2+1:b+sq1-1] = 0
     q[b:b+sq1-1] += q1[1:sq1]
@@ -46,12 +49,16 @@ function recvec(v; compute=BigInt, output=Int64)
     end
     q1, q2, q = q2, q, q1
     sq1, sq2 = sq2, sq
+    # substract term of degree 0 before computing next reciprocal
     l[1] = 0
+    b = 2
   end
-  return []
+  return Array(output, 0)
 
 end
 
-println(recvec([1,1,2,3,5,8,13,21], compute=Int64, output=Int32))
-println(recvec([1,2,4,8,16,32,64]))
-println(recvec([64,32,16,8,4,2,1]))
+# println(recvec([1,1,2,3,5,8,13,21], compute=Int64, output=Int32))
+# println(recvec([1,2,4,8,16,32,64]))
+# println(recvec([64,32,16,8,4,2,1]))
+# println("Failure: ",recvec([64,32,16,8,4,2,1,17]))
+# println("Failure: ",recvec([64]))
